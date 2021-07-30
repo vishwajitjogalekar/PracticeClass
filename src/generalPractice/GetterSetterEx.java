e1bc2344b40081c84b2b49121cceee13380d74c0
Vishwajit Jogalekar <vishwajitjogalekar@gmail.com>
2021-07-31 00:17:08 +0530

    new Code addition

---
+++ src/generalPractice/GetterSetterEx.java
@@ -0,0 +1,12 @@
+package generalPractice;
+
+public class GetterSetterEx {
+
+	public static void main(String[] args) {
+		User obj =new User();
+		obj.setUserId(100);
+		System.out.println(obj.getUserId());
+
+	}
+
+}
---
+++ src/generalPractice/User.java
@@ -0,0 +1,16 @@
+package generalPractice;
+
+public class User {
+
+	int userId=50;
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	
+}
