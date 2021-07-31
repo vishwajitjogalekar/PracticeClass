dbe0bd02040b9b1b74594c59a9623a80302da034
Vishwajit Jogalekar <vishwajitjogalekar@gmail.com>
2021-07-31 22:01:05 +0530

    Bubble sort Code additon

--- src/generalPractice/Collection1.java
+++ src/generalPractice/Collection1.java
@@ -1,9 +1,15 @@
 package generalPractice;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Collection1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		List <Integer> data=Arrays.asList(10,8,1,4,33,45);
+		
 
 	}
 
--- src/sortingTechnics/BubbleSort.java
+++ src/sortingTechnics/BubbleSort.java
@@ -1,10 +1,48 @@
 package sortingTechnics;
 
+import java.util.Scanner;
+
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		int size = 0;
+		int temp=0;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter size of data");
+		size=sc.nextInt();
+		int[] data =new int[size];
+		
+		// Enter data into array
+		System.out.println("Enter data to sort");
+		for (int i = 0; i < data.length; i++) {
+			data[i]=sc.nextInt();
+		}
+		
+		//Bubble Sorting Algorithm
+		for (int i = 0; i < data.length-1; i++) 
+		{
+			
+			for (int j = 0; j < data.length-1; j++) 
+			{
+				if(data[j]<data[j+1])
+				{
+					continue;
+				}
+				else
+				{
+					temp=data[j+1];
+					data[j+1]=data[j];
+					data[j]=temp;
+				}
+			}
+		}
+		
+		// Printing Sorted Array
+		System.out.print("Sorted Array is:");
+		for (int i = 0; i < data.length; i++) {
+			System.out.print(data[i]);
+		}
 	}
 
 }
